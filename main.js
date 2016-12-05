8b16310a82eba8acaefce24ea18c816090e68c13
blckt <denis.sadilo@nixsolutions.com>
2016-12-05 18:49:59 +0200

    changes

--- main.js
+++ main.js
@@ -16,86 +16,86 @@ require('electron-reload')(__dirname + '/build');
 let mainWindow, menu, dockMenu;
 
 let createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    toggleFileTasks(false);
-    toggleNewWindowTask(true);
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        toggleFileTasks(false);
+        toggleNewWindowTask(true);
 
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 
-  if (!menu) setMenu();
-  if (!dockMenu) setDock();
+    if (!menu) setMenu();
+    // if (!dockMenu) setDock();
 
-  toggleFileTasks(true);
-  toggleNewWindowTask(false);
-}
+    toggleFileTasks(true);
+    toggleNewWindowTask(false);
+};
 
 let toggleFileTasks = isEnabled => {
-  // The 'File' menu should only be available if there is an open window
-  menu.items
-    .find(item => item.label === 'File')
-    .submenu.items
-    .forEach(subItem => subItem.enabled = isEnabled);
-}
+    // The 'File' menu should only be available if there is an open window
+    menu.items
+        .find(item => item.label === 'File')
+        .submenu.items
+        .forEach(subItem => subItem.enabled = isEnabled);
+};
 
 let toggleNewWindowTask = isEnabled => {
-  // The 'New Window' task in the main menu and dock menu
-  // should only be available if there are no open windows
-  let newWindowMenu = menu.items
-    .find(item => item.label === 'Window')
-    .submenu.items
-    .find(subItem => subItem.label === 'New');
+    // The 'New Window' task in the main menu and dock menu
+    // should only be available if there are no open windows
+    let newWindowMenu = menu.items
+        .find(item => item.label === 'Window')
+        .submenu.items
+        .find(subItem => subItem.label === 'New');
 
-  let dockWindowMenu = dockMenu.items
-    .find(item => item.label === 'New Window');
+    // let dockWindowMenu = dockMenu.items
+    //     .find(item => item.label === 'New Window');
 
-  newWindowMenu.enabled = isEnabled;
-  dockWindowMenu.enabled = isEnabled;
-}
+    newWindowMenu.enabled = isEnabled;
+    // dockWindowMenu.enabled = isEnabled;
+};
 
 let setMenu = () => {
-  // Set custom click handlers for menu tasks
-  let fileMenu = menuTemplate
-    .find(item => item.label === 'File');
+    // Set custom click handlers for menu tasks
+    let fileMenu = menuTemplate
+        .find(item => item.label === 'File');
 
-  fileMenu.submenu
-    .find(item => item.label === 'Open')
-    .click = () => mainWindow.webContents.send('open-file')
+    fileMenu.submenu
+        .find(item => item.label === 'Open')
+        .click = () => mainWindow.webContents.send('open-file');
 
-  fileMenu.submenu
-    .find(item => item.label === 'Save As...')
-    .click = () => mainWindow.webContents.send('save-file')
+    fileMenu.submenu
+        .find(item => item.label === 'Save As...')
+        .click = () => mainWindow.webContents.send('save-file');
 
-  menuTemplate
-    .find(item => item.label === 'Window')
-    .submenu
-    .find(subItem => subItem.label === 'New')
-    .click = () => createWindow()
+    menuTemplate
+        .find(item => item.label === 'Window')
+        .submenu
+        .find(subItem => subItem.label === 'New')
+        .click = () => createWindow();
 
-  menu = Menu.buildFromTemplate(menuTemplate);
-  Menu.setApplicationMenu(menu);
-}
+    menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+};
 
-let setDock = () => {
-  // Create a 'New Window' task in the dock menu (OSX only)
-  dockMenu = Menu.buildFromTemplate([
-    { label: 'New Window', click: createWindow }
-  ]);
-  app.dock.setMenu(dockMenu);
-}
+// let setDock = () => {
+//   // Create a 'New Window' task in the dock menu (OSX only)
+//   dockMenu = Menu.buildFromTemplate([
+//     { label: 'New Window', click: createWindow }
+//   ]);
+//   app.dock.setMenu(dockMenu);
+// }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -103,15 +103,15 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow();
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,55 +3,56 @@ var webpack = require('webpack');
 var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
 module.exports = {
-  devtool: 'source-map',
-  debug: true,
+    devtool: 'source-map',
+    debug: true,
 
-  entry: {
-    '@angular': [
-      'rxjs',
-      'reflect-metadata',
-      'zone.js'
+    entry: {
+        '@angular': [
+            'rxjs',
+            'reflect-metadata',
+            'zone.js'
+        ],
+        'common': ['es6-shim'],
+        'app': './src/app/index.ts'
+    },
+
+    output: {
+        path: __dirname + '/build/',
+        publicPath: 'build/',
+        filename: '[name].js',
+        sourceMapFilename: '[name].js.map',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    resolve: {
+        extensions: ['', '.ts', '.js', '.json', '.css', '.html']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: ['./src'],
+                exclude: [/node_modules/, /releases/]
+            },
+            {
+                test: /\.json$/,
+                loader: 'json'
+            },
+            {
+                test: /\.(css|html)$/,
+                loader: 'raw'
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url?limit=10000'
+            }
+        ]
+    },
+
+    plugins: [
+        new CommonsChunkPlugin({ names: ['@angular', 'common'], minChunks: Infinity })
     ],
-    'common': ['es6-shim'],
-    'app': './src/app/app.ts'
-  },
-
-  output: {
-    path: __dirname + '/build/',
-    publicPath: 'build/',
-    filename: '[name].js',
-    sourceMapFilename: '[name].js.map',
-    chunkFilename: '[id].chunk.js'
-  },
-
-  resolve: {
-    extensions: ['','.ts','.js','.json', '.css', '.html']
-  },
-
-  module: {
-    loaders: [
-      {
-        test: /\.ts$/,
-        loader: 'ts',
-        exclude: [ /node_modules/, /releases/ ]
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
-      },
-      {
-        test: /\.(css|html)$/,
-        loader: 'raw'
-      },
-      {
-        test: /\.(png|jpg)$/,
-        loader: 'url?limit=10000'
-      }
-    ]
-  },
-
-  plugins: [
-    new CommonsChunkPlugin({ names: ['@angular', 'common'], minChunks: Infinity })
-  ],
-  target:'electron-renderer'
+    target: 'electron-renderer'
 };
